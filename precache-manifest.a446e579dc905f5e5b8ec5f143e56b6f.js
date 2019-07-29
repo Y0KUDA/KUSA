08376b57d73eb8f1ca6d2528a1e5365ce269f765
Y0KUDA <40825115+Y0KUDA@users.noreply.github.com>
2019-07-30 02:55:06 +0900

    tmp

---
+++ precache-manifest.a446e579dc905f5e5b8ec5f143e56b6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2c7b69c724c2ed2172f52228b1cd91",
+    "url": "/KUSA/index.html"
+  },
+  {
+    "revision": "62df11aae22ffcdedb35",
+    "url": "/KUSA/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/KUSA/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "62df11aae22ffcdedb35",
+    "url": "/KUSA/static/js/main.02400dc3.chunk.js"
+  },
+  {
+    "revision": "28096eb6a8844961d63e",
+    "url": "/KUSA/static/js/runtime~main.86c2c7d5.js"
+  }
+]);
